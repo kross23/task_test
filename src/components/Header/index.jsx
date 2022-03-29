b3d36b33d46b377981127c5563f5ae5394c05945
Cross <662242@mail.ru>
2022-03-29 23:05:35 +0500

    start

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div >
+            <h1>Наши топ-блогеры</h1>
+            <span>
+            Лучше специалисты в своем деле, 
+средний опыт работы в профессии - 27 лет
+            </span>
+        </div>
+    );
+}
+
+export default Index;
---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Arrow from '../Arrow';
+const Index = () => {
+    return (
+        <div>
+            <Arrow/>
+        </div>
+    );
+}
+
+export default Index;
